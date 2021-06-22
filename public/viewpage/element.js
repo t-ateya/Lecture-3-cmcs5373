501409d887e3f6aeb19f0f86589c680f49b62c99
Terence Ateya <ateyaterence@gmail.com>
2021-06-21 20:09:14 -0500

    Sign in modal form completed-video 9

---
+++ public/viewpage/element.js
@@ -0,0 +1,7 @@
+//top menus
+export const menuSignIn = document.getElementById('menu-signin');
+export const menuHome = document.getElementById('menu-home');
+export const menuPurchases = document.getElementById('menu-purchases');
+export const menuSignOut = document.getElementById('menu-signout');
+export const menuCart = document.getElementById('menu-cart');
+export const menuProfile = document.getElementById('menu-profile');
